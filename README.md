e048bd12a301bfd63b00ec35db42c13d8d449d19
juby210-PL <juby210@vp.pl>
2019-02-06 01:13:51 +0100

    Mutal Guilds Counter Plugin

--- README.md
+++ README.md
@@ -22,6 +22,11 @@ Send embed as user account. Use at own risk! | [Download](https://raw.githubuser
 ## Spotify Crack
 Spotify listen along without premium! | [Download](https://raw.githubusercontent.com/juby210-PL/EnhancedDiscord-plugins/master/spotify_crack.js)
 
+## Mutal Guilds Counter
+Mutal guilds counter | [Download](https://raw.githubusercontent.com/juby210-PL/EnhancedDiscord-plugins/master/mutal_guilds_counter.js)
+
+![Counter](https://i.imgur.com/TtSpowI.png)
+
 ## Binary Converter
 Binary converter plugin. | [Download](https://raw.githubusercontent.com/juby210-PL/EnhancedDiscord-plugins/master/binary_converter.js)
 
---
+++ mutal_guilds_counter.js
@@ -0,0 +1,29 @@
+const Plugin = require('../plugin');
+
+module.exports = new Plugin({
+    name: 'Mutal Guilds Counter',
+    author: 'Juby210#2100',
+    description: 'Mutal guilds counter',
+    color: 'aqua',
+
+    load: () => {
+        document.body.addEventListener('DOMNodeInserted', listener);
+    },
+    unload: () => {
+        document.body.removeEventListener('DOMNodeInserted', listener);
+    }
+});
+
+function listener(e) {
+    if(!e.target || !e.target.classList) return;
+    let el = e.target;
+    
+    if (el.classList.contains("scrollerWrap-2lJEkd") && Array.from(document.body.getElementsByClassName("item-PXvHYJ"))[1].classList.contains("itemSelected-1qLhcL")) {
+        //console.log(el);
+        let scroller = Array.from(el.getElementsByClassName("scroller-2FKFPG"))[0];
+        let count = Array.from(scroller.getElementsByClassName("listRow-hutiT_")).length;
+        let elm = Array.from(document.body.getElementsByClassName("item-PXvHYJ"))[1];
+        if(elm.innerText.includes(count)) return;
+        elm.innerText += ` [${count}]`;
+    }
+}
