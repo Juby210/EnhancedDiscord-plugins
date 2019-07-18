c38f4a0fc4807013f90d0d2e0081719a2dfab74d
juby210-PL <juby210@vp.pl>
2019-07-18 19:55:44 +0200

    Rewrite Unread Servers Plugin

--- unread_servers.js
+++ unread_servers.js
@@ -6,29 +6,32 @@ module.exports = new Plugin({
     author: "Juby210#2100",
     description: "Open unread server list on ctrl+s",
     color: "#ff0000",
-    JubyLib: JubyLib,
+    version: 1.0,
 
     load: async () => {
         await module.exports.loadLib();
         document.addEventListener("keyup", module.exports.onkey);
+        JubyLib.injectCSS('uspcss', `.uspsico {height: 48px; width: 48px; border-radius: 50%; display: inline;}`)
+        JubyLib.updatesModule.checkUpdate("https://raw.githubusercontent.com/juby210-PL/EnhancedDiscord-plugins/master/plugins_versions.json", module.exports, "https://raw.githubusercontent.com/juby210-PL/EnhancedDiscord-plugins/master/unread_servers.js");
 	},
     unload: () => {
-        document.removeEventListener("keyup", module.exports.onkey);
+        document.removeEventListener("keyup", module.exports.onkey)
+        JubyLib.clearCSS('uspcss')
     },
 
     onkey: e => {
-        let c = findModules('pill')[3];
         if (e.ctrlKey && e.key == 's') {
-            let content = '';
-            $(`.${c.pill.split(' ')[0]}`).each((i, p) => {
-                if($(p).children().first().attr('style') == 'opacity: 0.7; height: 8px; transform: translate3d(0px, 0px, 0px);') {
-                    let el = $(p).parent().find(`.${c.blobContainer.split(' ')[0]}`).clone();
-                    let h = $(el).find('.wrapper-1BJsBx').attr('href').split('/');
-                    $(el).find('.wrapper-1BJsBx').attr('href', `javascript:findModule('selectChannel').selectChannel('${h[2]}','${h[3]}');`);
-                    content += `<div style="color:white"><div style="display:inline-block">${$(el).html()}</div><h1 style="display:inline;margin-left:5px">${$(el).find('.wrapper-1BJsBx').attr('aria-label')}</h1></div>`;
+            let gs = findModule('getGuilds').getGuilds()
+            let hu = findModule('getGuildUnreadCount').hasUnread
+            let content = ''
+            Object.keys(gs).forEach(k => {
+                let g = gs[k]
+                if(hu(g.id)) {
+                    content += `<div style="color:white"><a href="javascript:findModules('selectChannel')[1].selectChannel('${g.id}')"><div>
+                        <img src="https://cdn.discordapp.com/icons/${g.id}/${g.icon}" class="uspsico"></img><h1 style="display:inline-block;margin-left:5px;color:white">${g.name}<h1></div></a></div>`
                 }
-            });
-            JubyLib.popup("Unread servers", content);
+            })
+            JubyLib.popup("Unread Servers", content)
         }
     },
     loadLib: () => {
