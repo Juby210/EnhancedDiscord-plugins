2967cb56f8531fbc2ca5936601c2a1478d9d6bd4
juby210-PL <juby210@vp.pl>
2019-08-05 22:35:16 +0200

    Select unread channel / Unread Servers

--- unread_servers.js
+++ unread_servers.js
@@ -6,7 +6,7 @@ module.exports = new Plugin({
     author: "Juby210#2100",
     description: "Open unread server list on ctrl+s",
     color: "#ff0000",
-    version: 1.0,
+    version: 1.01,
 
     load: async () => {
         await module.exports.loadLib();
@@ -22,17 +22,27 @@ module.exports = new Plugin({
     onkey: e => {
         if (e.ctrlKey && e.key == 's') {
             let gs = findModule('getGuilds').getGuilds()
+            let chu = findModule('hasUnread').hasUnread
             let hu = findModule('getGuildUnreadCount').hasUnread
+            let channels = module.exports.objToArray(findModule('getChannels').getChannels())
             let content = ''
             Object.keys(gs).forEach(k => {
                 let g = gs[k]
                 if(hu(g.id)) {
-                    content += `<div style="color:white"><a href="javascript:findModules('selectChannel')[1].selectChannel('${g.id}')"><div>
+                    let cid = channels.filter(c => c.guild_id == g.id && chu(c.id))[0].id
+                    content += `<div style="color:white"><a href="javascript:findModules('selectChannel')[1].selectChannel('${g.id}','${cid}')"><div>
                         <img src="https://cdn.discordapp.com/icons/${g.id}/${g.icon}" class="uspsico"></img><h1 style="display:inline-block;margin-left:5px;color:white">${g.name}<h1></div></a></div>`
                 }
             })
             JubyLib.popup("Unread Servers", content)
         }
+    },
+    objToArray: obj => {
+        let arr = []
+        Object.keys(obj).forEach(k => {
+            arr.push(obj[k])
+        })
+        return arr
     },
     loadLib: () => {
         return new Promise((resolve, reject) => {
