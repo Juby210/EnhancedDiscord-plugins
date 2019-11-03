7b1be292ed2c65030672c36332650305c743b9ff
juby210-PL <juby210@vp.pl>
2019-11-03 10:44:54 +0100

    Rewrite to react

--- unread_servers.js
+++ unread_servers.js
@@ -1,59 +1,62 @@
-const Plugin = require("../plugin");
-let JubyLib;
+const Plugin = require('../plugin')
+const { React, React: { createElement: e }, injectCSS, clearCSS,
+    findModuleByDisplayName, findModuleByProps } = EDApi
 
 module.exports = new Plugin({
     name: "Unread Servers",
     author: "Juby210#2100",
     description: "Open unread server list on ctrl+s",
     color: "#ff0000",
-    version: 1.02,
 
-    load: async () => {
-        await module.exports.loadLib();
-        document.addEventListener("keyup", module.exports.onkey);
-        JubyLib.injectCSS('uspcss', `.uspsico {height: 48px; width: 48px; border-radius: 50%; display: inline;}`)
-        JubyLib.updatesModule.check("https://raw.githubusercontent.com/juby210-PL/EnhancedDiscord-plugins/master/plugins_versions.json", module.exports, "https://raw.githubusercontent.com/juby210-PL/EnhancedDiscord-plugins/master/unread_servers.js");
+    load: () => {
+        document.addEventListener("keyup", module.exports.onkey)
+        injectCSS('uspcss', `.uspsico {height: 48px; width: 48px; border-radius: 50%; display: inline;}`)
 	},
     unload: () => {
         document.removeEventListener("keyup", module.exports.onkey)
-        JubyLib.clearCSS('uspcss')
+        clearCSS('uspcss')
     },
 
     onkey: e => {
         if (e.ctrlKey && e.key == 's') {
-            let gs = findModule('getGuilds').getGuilds()
-            let chu = findModule('hasUnread').hasUnread
-            let hu = findModule('getGuildUnreadCount').hasUnread
-            let channels = module.exports.objToArray(findModule('getChannels').getChannels())
-            let content = ''
-            Object.keys(gs).forEach(k => {
-                let g = gs[k]
-                if(hu(g.id)) {
-                    let cid = channels.filter(c => c.guild_id == g.id && chu(c.id))
-                    let cid2 = cid.filter(c => !findModule('isMuted').isChannelMuted(g.id, c.id))[0]
-                    if(cid2) cid = cid2.id;
-                    else cid = cid[0].id;
+            const gs = findModule('getGuilds').getGuilds()
+            const hu = findModule('getGuildUnreadCount').hasUnread
+            const guilds = Object.values(gs).filter(g => hu(g.id))
 
-                    content += `<div style="color:white"><a href="javascript:findModules('selectChannel')[1].selectChannel('${g.id}','${cid}')"><div>
-                        <img src="https://cdn.discordapp.com/icons/${g.id}/${g.icon}" class="uspsico"></img><h1 style="display:inline-block;margin-left:5px;color:white">${g.name}<h1></div></a></div>`
-                }
-            })
-            JubyLib.popup("Unread Servers", content)
+            findModule('push').push(module.exports.Modal, { guilds })
         }
     },
-    objToArray: obj => {
-        let arr = []
-        Object.keys(obj).forEach(k => {
-            arr.push(obj[k])
-        })
-        return arr
-    },
-    loadLib: () => {
-        return new Promise((resolve, reject) => {
-            require('request')({url:`https://juby210-pl.github.io/EnhancedDiscord-plugins/lib/JubyLib.js`},(err,res,body)=> {
-                JubyLib = eval(body);
-                resolve();
-            });
-        });
+
+    Modal: class extends React.Component {
+        render() {
+            const hc = findModuleByProps('header', 'botTag', 'listAvatar')
+            const chu = findModule('hasUnread').hasUnread
+            const channels = Object.values(findModule('getChannels').getChannels())
+            const { selectChannel } = findModules('selectChannel')[1]
+
+            const Modal = findModule('CloseButton')
+            const Title = findModuleByDisplayName('FormTitle')
+            const Scroller = findModuleByDisplayName('VerticalScroller')
+            const IconWrapper = findModuleByDisplayName('GuildIconWrapper')
+
+            const sc = e(Scroller, { children: [], style: { 'margin-top': '5px' } })
+            this.props.guilds.forEach(g => {
+                let cid = channels.filter(c => c.guild_id == g.id && chu(c.id))
+                let cid2 = cid.find(c => !findModule('isMuted').isChannelMuted(g.id, c.id))
+                if(cid2) cid = cid2.id;
+                else cid = cid[0].id;
+
+                sc.props.children.push(e('div', { className: hc.listRow, onClick: () => { selectChannel(g.id, cid); this.props.onClose() } },
+                    e(IconWrapper, { guild: g }),
+                    e('div', { className: hc.listRowContent, style: { 'margin-left': '5px' } }, e('div', { className: hc.listName }, g.name))
+                ))
+            })
+
+            return e(Modal, {},
+                e(Modal.Header, {}, e(Title, { tag: 'h4' }, 'Unread Servers'),
+                    e(Modal.CloseButton, { onClick: this.props.onClose })
+                ), sc
+            )
+        }
     }
 });
