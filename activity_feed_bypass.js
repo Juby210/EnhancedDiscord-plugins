7b97c58631ffdb74e4ddbd8a45e7c8f3d6a3bcba
Juby210 <juby210@vp.pl>
2020-01-23 15:05:40 +0100

    new people tab / Activity Tab Bypass

--- activity_feed_bypass.js
+++ activity_feed_bypass.js
@@ -1,4 +1,4 @@
-const Plugin = require('../plugin');
+const Plugin = require('../plugin')
 
 module.exports = new Plugin({
     name: 'Activity Feed Bypass',
@@ -7,12 +7,14 @@ module.exports = new Plugin({
     color: '#7289da',
 
     load: () => {
-        monkeyPatch(findModule('hasConsented'), 'hasConsented', b => {
-            if(b.methodArguments[0] == 'personalization' && window.location.href.split('/')[3] == 'activity') return true
+        this.unpatch = monkeyPatch(findModule('hasConsented'), 'hasConsented', b => {
+            if(b.methodArguments[0] == 'personalization' &&
+            (window.location.pathname == '/activity' ||
+            window.location.pathname == '/channels/@me')) return true
             return b.callOriginalMethod(b.methodArguments[0])
         })
     },
     unload: () => {
-        findModule('hasConsented').hasConsented.unpatch()
+        if(this.unpatch) this.unpatch()
     }
-});
+})
